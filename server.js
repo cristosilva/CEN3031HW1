bdecb380f4279e13763e7e6327253a33c93af701
cristosilva <cristo.silva@yahoo.com>
2017-08-29 12:25:34 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,12 @@ var listingData, server;
 var requestHandler = function(request, response) {
     var parsedUrl = url.parse(request.url);
 //me:
+    //if url has /listings, do a 200 code and write the listingData to the server
     if( parsedUrl.pathname == '/listings'){
       response.writeHead(200, {'Content-Type': 'text/plain'});
       response.end(listingData);
     }
+    //if url does not havee /listings, do a 404 code and display Bad gateway error.
     else{
       response.writeHead(404);
       response.end("Bad gateway error");
@@ -28,14 +30,14 @@ var requestHandler = function(request, response) {
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
   //me
+    //saves the data from the readFile to listingData then creates the server
     listingData = data;
     server = http.createServer(requestHandler);
-    // the server is now started, listening for requests on port 8080
+    //the server is now started
     server.listen(port, function() {
-    //once the server is listening, this callback function is executed
-    console.log('Server listening on: http://127.0.0.1:' + port);
+    console.log('Server listening');
     });
-    console.log('Is the server started?');
+    console.log('Server started?');
     //end me
     /*
     This callback function should save the data in the listingData variable, 
